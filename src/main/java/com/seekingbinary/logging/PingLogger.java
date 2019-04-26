907bf203eeee5047fcafb99408ff893ebbeac257
Alex Lewis <alex.lewis001@gmail.com>
2019-04-26 13:18:24 +0100

    Add SLF4J Logging

--- src/main/java/com/seekingbinary/logging/PingLogger.java
+++ src/main/java/com/seekingbinary/logging/PingLogger.java
@@ -4,13 +4,23 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @Path("ping")
 public class PingLogger {
   
+  private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+  
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   public String ping() {
+    LOG.debug("pong: Debug");
+    LOG.warn("pong: Warn");
+    LOG.error("pong: Error");
+    LOG.trace("pong: Trace");
+    LOG.info("pong: Info");
     return "pong";
   }
 }
